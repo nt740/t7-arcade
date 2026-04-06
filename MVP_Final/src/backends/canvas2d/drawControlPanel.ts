db990e4a54335f09b66a7f1aeb9be4c0feee9be8
nt740 <summer_tunes_2j@icloud.com>
2026-04-07 00:39:06 +0100

    Delete MVP_Final directory

--- MVP_Final/src/backends/canvas2d/drawControlPanel.ts
+++
@@ -1,8 +0,0 @@
-import type { DebugSnapshot } from "../../core/types/debug";
-
-export const drawControlPanel = (
-  _context: CanvasRenderingContext2D,
-  _debugSnapshot: DebugSnapshot
-): void => {
-  // Phase 1 uses DOM controls only.
-};
